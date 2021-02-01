a1a97303fac947c15270a8013b30d438a0ff3386
Ivan Fyodor <aamerson198@gmail.com>
2021-02-01 14:17:47 -0500

    added code for get routes and view routes to /notes

--- routes/APIroutes.js
+++ routes/APIroutes.js
@@ -5,3 +5,14 @@ const db = require("../db/db.json");
 const util = require("util");
 const writeFileSync = util.promisify(fs.writeFile);
 const readFile = fs.readFile;
+
+module.exports = (app) => {
+
+    app.get("/api/notes", (req, res) => {
+        readFile("./db/db.json", (error, data) => {
+            let db = JSON.parse(data);
+            if (error) throw console.log(error);
+            res.json(db);
+        });
+    });
+}
