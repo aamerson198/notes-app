f0f9ac20eaa912e5d3d2f20a45187b9f10775481
Ivan Fyodor <aamerson198@gmail.com>
2021-02-01 16:20:13 -0500

    finished routes to get app working

--- routes/APIroutes.js
+++ routes/APIroutes.js
@@ -7,7 +7,7 @@ const writeFileSync = util.promisify(fs.writeFile);
 const readFile = fs.readFile;
 
 module.exports = (app) => {
-
+    // API Routes
     app.get("/api/notes", (req, res) => {
         readFile("./db/db.json", (error, data) => {
             let db = JSON.parse(data);
@@ -15,4 +15,37 @@ module.exports = (app) => {
             res.json(db);
         });
     });
-}
+    // Route to post a new note
+    app.post("/api/notes", (req, res) => {
+        // console.log(db);
+        let note = req.body;
+        // console.log(note);
+        let id = uuidv4();
+        note.id = id;
+        // console.log(db);
+        db.push(note);
+        writeFileSync("./db/db.json", JSON.stringify(db))
+            .then(() => {
+                res.json(note);
+            })
+            .catch((err) => console.error(err));
+    });
+    // Route to delete a note
+    app.delete("/api/notes/:id", (req, res) => {
+        // Once note is deleted, it will disappear from the notes list
+        readFile("./db/db.json", (error, data) => {
+            let db = JSON.parse(data);
+            let newNote = db.filter((note) => note.id != req.params.id);
+            console.log(newNote);
+            const updateNote = newNote;
+            writeFileSync("./db/db.json", JSON.stringify(updateNote))
+                .then(() => {
+                    res.json(updateNote);
+                })
+                .catch((err) => {
+                    console.error(err);
+                });
+        });
+
+    });
+};
