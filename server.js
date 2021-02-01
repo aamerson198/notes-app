25684bf021694e3432664085f4ff5a3fc04a8b39
Ivan Fyodor <aamerson198@gmail.com>
2021-02-01 10:18:29 -0500

    added server.js and filled out with boiler plate express set up.

---
+++ server.js
@@ -0,0 +1,23 @@
+//  Require Dependencies
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+// Create an instance of express
+const app = express();
+
+// Set PORT
+const PORT = process.env.PORT || 8080;
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+// Require routes file
+require("./routes/APIroutes")(app);
+require("./routes/viewRoutes")(app);
+
+// Listen on the PORT
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
